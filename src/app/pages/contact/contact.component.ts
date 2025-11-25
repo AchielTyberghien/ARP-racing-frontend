479af9fa734746b3f9966001f812ac482f6df415
Achiel Tyberghien <achiel.tyberghien@student.vives.be>
2025-11-25 19:03:47 +0100

    opbouw email contact form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormControlDirective, FormDirective, FormLabelDirective } from '@coreui/angular';
 
 @Component({
   selector: 'app-contact',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,ReactiveFormsModule,
+    FormsModule,
+    FormDirective,
+    FormLabelDirective,
+    FormControlDirective],
   template: `
     <app-header></app-header>
+    
+    <div class="m-10 flex justify-center flex-wrap">
+      <h2 class="w-150">Contact</h2>
+      <form cForm class="w-150">
+        <div class="mb-3">
+          <label cLabel for="exampleFormControlInput1">Email address</label>
+          <input cFormControl
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+                type="email"
+          />
+        </div>
+        <div class="mb-3">
+          <label cLabel for="exampleFormControlInput2">Subject</label>
+          <input cFormControl
+                id="exampleFormControlInput2"
+                placeholder="Subject"
+                type="text"
+          />
+        </div>
+        <div class="mb-3">
+          <label cLabel for="exampleFormControlTextarea1">Example textarea</label>
+          <textarea cFormControl id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+      </form>
+    </div>
+    
   `,
   styles: ``,
 })
