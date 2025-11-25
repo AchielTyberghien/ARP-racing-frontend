73739f69cd074505ff7c4ef49614215e6bd62d7e
Achiel Tyberghien <achiel.tyberghien@student.vives.be>
2025-11-25 16:21:56 +0100

    picflow portfolio toevoegen

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,30 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
 import { TitleComponent } from '../../components/title/title.component';
 
 @Component({
   selector: 'app-portfolio',
   imports: [TitleComponent, ToolbarComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <div class="p-2 top-0 left-0 w-full bg-black" >
       <app-title title="ARP Racing Photography"/>
       <app-toolbar/>
     </div>
-    <div class="p-4 flex">
-      <div class="m-2">
-        <img src="img/portfolio/IMG_7786.jpg" alt="" class="mb-3">
-        <img src="img/portfolio/IMG_8048.jpg" alt="">
-      </div>
-      <div class="m-2">
-        <img src="img/portfolio/IMG_7916.jpg" alt="" class="mb-3">
-        <img src="img/portfolio/IMG_8408.jpg" alt="">
-      </div>
-      <div class="m-2">
-        <img src="img/portfolio/IMG_8400.jpg" alt="" class="mb-3">
-        <img src="img/portfolio/IMG_8223.jpg" alt="" class="mb-3">
-        <img src="img/portfolio/IMG_8076.jpg" alt="">
-      </div>
-    </div>
+    <picflow-gallery 
+      id="gal_LV7QSi90Qs9M1uzf"
+      tenant="tnt_1IHEoA81RTBoPxjS"
+      lightbox="#000000E6"
+      no-padding="true"
+      no-background="true"
+      show-cover="true"
+      class="p-3">
+    </picflow-gallery>
+
   `,
   styles: ``,
 })
