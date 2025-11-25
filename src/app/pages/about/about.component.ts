cd27ee70f8264d959bd3483323805814d29b091f
Achiel Tyberghien <achiel.tyberghien@student.vives.be>
2025-11-25 09:52:21 +0100

    maken about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -10,9 +10,14 @@ import { ToolbarComponent } from '../../components/toolbar/toolbar.component';
       <app-title title="ARP Racing Photography"/>
       <app-toolbar/>
     </div>
-    <div>
-      <div></div>
-      <div></div>
+    <div class="flex p-10">
+      <div>
+        <img src="img/about/portret.jpg" alt="portret" class="w-60 h-60 rounded-full mx-auto mt-4"/>
+      </div>
+      <div>
+        <p class="text-center text-lg mt-4 mx-4">
+          Welcome to ARP Racing Photography! I'm Achiel, a passionate motorsport photographer dedicated to capturing the thrill and excitement of racing events.
+      </div>
     </div>
   `,
   styles: ``,
