239f1148cdd299b64258ff1894be15c23954651c
Achiel Tyberghien <achiel.tyberghien@student.vives.be>
2025-11-25 17:05:10 +0100

    nav buttons

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-title',
-  imports: [],
+  imports: [RouterLink],
   template: `
-      <div class="text-center text-3xl font-bold text-white font-helvetica "> {{title()}} </div>
+      <div class="text-center text-3xl font-bold text-white font-helvetica " routerLink=""> {{title()}} </div>
   `,
   styles: ``
 })
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [RouterLink],
   template: `
     <div class="px-[15%] py-4 flex justify-between items-center" >
-      <div class="text-lg font-semibold text-white font-helvetica">About</div>
-      <div class="text-lg font-semibold text-white font-helvetica">Portfolio</div>
-      <div class="text-lg font-semibold text-white font-helvetica">Library</div>
-      <div class="text-lg font-semibold text-white font-helvetica">Contact</div>
+      <div class="text-lg font-semibold text-white font-helvetica" routerLink="/about">About</div>
+      <div class="text-lg font-semibold text-white font-helvetica" routerLink="/portfolio">Portfolio</div>
+      <div class="text-lg font-semibold text-white font-helvetica" routerLink="/library">Library</div>
+      <div class="text-lg font-semibold text-white font-helvetica" routerLink="/contact">Contact</div>
     </div>
   `,
   styles: ``
