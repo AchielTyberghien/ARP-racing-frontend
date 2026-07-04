44c35f0b066abc127de440d022e01d81334047e8
Achiel Tyberghien <achiel.tyberghien@student.vives.be>
2026-07-04 10:01:45 +0200

    remove white border

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -5,24 +5,24 @@ import { RouterLink } from "@angular/router";
   selector: 'app-toolbar',
   imports: [RouterLink],
   template: `
-    <div class="w-full flex flex-col md:flex-row justify-center items-center
-            space-y-4 md:space-y-0 md:space-x-12
+    <div class=" flex flex-col md:flex-row justify-center items-center
+            space-y-4 md:space-y-0 md:space-x-10
             py-4 bg-transparent md:px-15">
 
       <div routerLink="/portfolio"
-          class="nav-link text-lg md:text-xl font-semibold font-helvetica
+          class="text-lg md:text-xl nav-underline font-semibold font-helvetica
                   transition-transform duration-300 hover:scale-110 cursor-pointer">
         Portfolio
       </div>
 
       <div routerLink="/library"
-          class="nav-link text-lg md:text-xl font-semibold font-helvetica
+          class="text-lg md:text-xl nav-underline font-semibold font-helvetica
                   transition-transform duration-300 hover:scale-110 cursor-pointer">
         Library
       </div>
 
       <div routerLink="/contact"
-          class="nav-link text-lg md:text-xl font-semibold font-helvetica
+          class="text-lg md:text-xl nav-underline font-semibold font-helvetica
                   transition-transform duration-300 hover:scale-110 cursor-pointer">
         Contact
       </div>
